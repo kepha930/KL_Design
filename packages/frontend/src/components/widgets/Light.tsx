669ca84b2ed8a5da1beb2bad455a7bcf5923e60f
Zuguang Tong <tongzuguang@gmail.com>
2024-06-10 18:47:32 +1000

    light

--- packages/frontend/src/components/widgets/Light.tsx
+++ packages/frontend/src/components/widgets/Light.tsx
@@ -10,21 +10,33 @@ const Light: FC = memo(() => {
                 position: 'relative',
                 width: '100%',
                 height: '3px',
+                overflow: 'hidden',
+                backgroundColor: '#272727',
             }}
         >
             <Box
                 sx={{
                     position: 'absolute',
                     bottom: '0px',
-                    height: '3px',
-                    width: '80px',
-                    '@keyframes moveLight': {
-                        '0%': { left: '0' },
-                        '100%': { left: 'calc(100% - 80px)' },
+                    height: '2px',
+                    width: {
+                        xs: '80px',
+                        lg: '140px',
+                    },
+                    '@keyframes moveLight_xs': {
+                        '0%': { left: '-140px' },
+                        '100%': { left: 'calc(100%)' },
+                    },
+                    '@keyframes moveLight_lg': {
+                        '0%': { left: '-200px' },
+                        '100%': { left: 'calc(100%)' },
                     },
                     backgroundImage:
                         `linear-gradient(to right, transparent, ${KL_Theme.palette.secondary.main}, transparent)`,
-                    animation: 'moveLight 5s linear infinite alternate',
+                    animation: {
+                        xs: 'moveLight_xs 4s ease-in-out infinite alternate',
+                        lg: 'moveLight_lg 4s ease-in-out infinite alternate',
+                    },
                 }}
             ></Box>
         </Box>
